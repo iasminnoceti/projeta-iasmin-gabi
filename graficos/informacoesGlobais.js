c8a536481a20cfb5ec0cf5c5e5f7f9939b7a9bde
iasminnoceti <129758500+iasminnoceti@users.noreply.github.com>
2024-10-24 11:04:14 -0300

    informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -11,7 +11,7 @@ async function vizualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Os números reunidos por We Are Social e Meltwater indicam que o Brasil tem<span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> de internautas em 2024.  <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
+    paragrafo.innerHTML = `Os números reunidos por We Are Social e Meltwater indicam que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> delas utilizam a internet por <span>${horas} horas</span> e <span>${minutos} minutos</span> por dia.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas são influenciadas pela rede.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
